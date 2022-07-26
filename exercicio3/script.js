b2d7ec0185f3ae2bdce09dad57667b39f17063ac
Wellerson-Custodio <wellerson.custodio@gmail.com>
2022-07-26 20:58:22 -0300

    exercício2 corrigido, exercício3 adicionado extra

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,4 +1,6 @@
 //Crie a const para a frase aqui
+
+//exercício 3
 const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\"`
 const novaFrase = frase.replace("verde","rosa")    
 const novaFrase2 = novaFrase.replace("azul","laranja")
@@ -6,6 +8,7 @@ const novaFrase2 = novaFrase.replace("azul","laranja")
 
 console.log(novaFrase2)
 
+//possui ou não verde laranja
 const detalhe = `a frase possui verde?`
 console.log(detalhe, novaFrase2.includes("verde"))
 
@@ -16,3 +19,4 @@ console.log(detalhe2, novaFrase2.includes("laranja"))
 const extra = `mas não deixe o gato sair`.toUpperCase()
 const extra2 = novaFrase2.replace("mas não deixe o gato sair",extra)
 console.log(extra2)
+
