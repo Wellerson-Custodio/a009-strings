bab50a428062ea175f47361cbe6f40b1439b54be
Wellerson-Custodio <108699299+Wellerson-Custodio@users.noreply.github.com>
2022-07-26 21:06:23 -0300

    exercicio concluidos (#1)
    * alterações feitas no exercício2 e no exercício3
    
    * exercício2 corrigido, exercício3 adicionado extra

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,11 @@
+
+//exercício 2
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString2 = minhaString.trim()
+const minhaString3 = minhaString2.replace("________","sticioso")
+
+//colocado o número de caracteres antes e depois de retirar os espaços sobresalentes.
+console.log(minhaString.length)
+console.log(minhaString3.length)
+
+console.log(minhaString3)
